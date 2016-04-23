4250b82b887f61e7e4d578e4eaefbe4b3a05c6b0
FeminismIsAwesome <ian.norris.1991@gmail.com>
2016-04-23 12:41:12 -0700

    testtt

--- test.js
+++ test.js
@@ -72,7 +72,7 @@ function thermometer(goalAmount, progressAmount, animate) {
 }
 
 function extractAmounts(goalAmt, previouslyMade, trackingTag) {
-  $.get({url: "https://powerful-plateau-77155.herokuapp.com/silly/" + trackingTag,
+  $.getJSON({url: "http://powerful-plateau-77155.herokuapp.com/silly/" + trackingTag,
    success: function(data) {
   var progress = parseInt(data.data)+540;
   var amounts = {
